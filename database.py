50f41fcbe072235a50d857788e56cc7b1cd8b649
AllyKisAlwaysTaken-jeez <alyshamktaha@gmail.com>
2025-11-17 00:44:36 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
-
 
 DB_URL = os.environ.get("DATABASE_URL", "sqlite:///ai_agent.db")
 
+# If using SQLite we need check_same_thread, for other DBs leave connect_args empty
+connect_args = {"check_same_thread": False} if DB_URL.startswith("sqlite") else {}
 
-engine = create_engine(DB_URL, connect_args={"check_same_thread": False} if DB_URL.startswith("sqlite") else {})
+engine = create_engine(DB_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
